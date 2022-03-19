97a487c457438939e58eb99009398ac0f8cecb2d
Curly <92770885+DevCurly@users.noreply.github.com>
2022-03-19 17:40:41 -0300

    A1.1
    Little changes and you can just enter url.com and it will automatically put https:// infront.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Chicken Burger browser
+
+#Made by @zDevCurly
+# https://www.github.com/DevCurly/Chicken-Burger-brws
+
 
 import sys
 from PyQt5.QtCore import *
@@ -5,7 +10,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 import os
 print("Welcome to Chicken Burger browser!")
-Home =  str(input("Home page: "))
+Home =  str(input("Home page (Example: url.com): "))
 Final = Home
 if Home.find("https://") == 0:
     Final = Home
@@ -16,7 +21,6 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-#        self.browser.setUrl(QUrl("/ver.js"))
         self.browser.setUrl(QUrl(Final))
         self.setCentralWidget(self.browser)
         self.showMaximized()
@@ -37,8 +41,7 @@ class MainWindow(QMainWindow):
         navbar.addAction(forward_btn)
         ver=""
 
-
-        ver = QAction("A1.0",self)
+        ver = QAction("A1.1",self)
         ver.triggered.connect(self.ripv)
         navbar.addAction(ver)
 
@@ -55,12 +58,18 @@ class MainWindow(QMainWindow):
         
         self.browser.urlChanged.connect(self.update_url)
     def ripv(self):
-        self.ver =""
+        self.ver =QAction("A1.1")
+
     def navigate_home(self):
         self.browser.setUrl(QUrl(Final))
     def navigate_to_url(self):
         url = self.url_bar.text()
-        self.browser.setUrl(QUrl(url))
+        __final__ = ""
+        if url.find("https://") == 0:
+            __final__ = url
+        else:
+            __final__ = "https://"+url
+        self.browser.setUrl(QUrl(__final__))
     def update_url(self, q):
         self.url_bar.setText(q.toString())
 
@@ -75,4 +84,5 @@ QApplication.setApplicationName("Chicken Burger")
 window = MainWindow()
 app.exec_()
 print("Made by @zDevCurly")
+print("https://www.github.com/DevCurly/Chicken-Burger-brws")
 print("---------------------------------------------------------------------------------")
